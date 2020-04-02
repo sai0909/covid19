f7a68bc05395ac70bd33ea18074131e216fd2e95
Sai Venkatesh <sai0909@gmail.com>
2020-04-02 23:20:31 +0530

    global card creation

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,36 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app-bar app flat color="black" dark>
+      <v-img
+        max-height="40"
+        max-width="40"
+        contain
+        :src="require('./assets/logo.png')"
+      ></v-img>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title class="ml-2">
+        Covid-19
+      </v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Dashboard />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Dashboard from "./components/Dashboard";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    Dashboard
   },
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
